4f1e089fd8d757320b9b8137f5c022d9a2b725bc
BabyBo Chen <tc10131@hotmail.com>
2023-03-08 23:56:36 +0800

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/home/Home.vue';
 import CryLaugh from '../views/cryLaugh/CryLaugh.vue';
 import LeftRight from '../views/leftRight/LeftRight.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
   routes: [
     {
       path: '/',
